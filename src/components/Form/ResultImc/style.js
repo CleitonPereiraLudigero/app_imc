721c9d3f2b7878899848e1849659efebfc7620c2
CleitonPereiraLudigero <c.ludigero@gmail.com>
2023-12-24 02:43:11 -0300

    primeiro commit

---
+++ src/components/Form/ResultImc/style.js
@@ -0,0 +1,51 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+    resultImc: {
+        flex:1,
+        marginTop: 10,
+        paddingTop:50,
+        borderRadius:50,
+        alignItems:"center",
+        width:"100%",
+        
+    },
+    numberImc:{
+        fontSize:48,
+        color:"#ff0043",
+        fontWeight:"bold",
+    },
+    information:{
+        fontSize:18,
+        color:"#ff0043",
+        fontWeight:"bold",
+
+    },
+    boxSharedButton:{
+        alignItems:"center",
+        marginBottom:10,
+        
+        
+        
+        
+    },
+    shared:{
+        backgroundColor:"#1877f2",
+        borderRadius:50,
+        paddingBottom: 5,
+        paddingTop: 5,
+        
+        
+        
+        
+    },
+    sharedText:{
+        color:"#ffffff",
+        fontWeight:"bold",
+        paddingHorizontal:45,
+        
+        
+    }
+    });
+
+export default styles
